64787836358542df4615eb29c23536d89c478bf2
Alex Hay <mralexhay@gmail.com>
2020-10-21 09:19:47 +0100

    Initial Commit

---
+++ ShortcutsExampleiOS14/ShortcutsExampleiOS14App.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutsExampleiOS14App.swift
+//  ShortcutsExampleiOS14
+//
+//  Created by Alex Hay on 21/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutsExampleiOS14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
